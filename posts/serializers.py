664e12c42967d48053e1adec0c3acf21c0a09a90
“Ivona” <ivona.rieger@gmail.com>
2025-06-02 17:41:52 +0200

    add comment filter and post

--- posts/serializers.py
+++ posts/serializers.py
@@ -28,9 +28,11 @@ class PostSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     def get_like_id(self, obj):
-        user = self.context["request"].user
+        user = self.context['request'].user
         if user.is_authenticated:
-            like = Like.objects.filter(owner=user, post=obj).first()
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
             return like.id if like else None
         return None
 
