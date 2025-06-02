1752ff1eae465275bb51db140454dce2b8698cae
“Ivona” <ivona.rieger@gmail.com>
2025-06-02 16:15:47 +0200

    add annotation to querysets

--- posts/serializers.py
+++ posts/serializers.py
@@ -14,21 +14,21 @@ class PostSerializer(serializers.ModelSerializer):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError(
                 "Image size larger than 2MB!"
-                )
+            )
         if value.image.height > 4096:
             raise serializers.ValidationError(
                 "Image height larger than 4096px!"
-                )
+            )
         if value.image.width > 4096:
             raise serializers.ValidationError(
                 "Image width larger than 4096px!"
-                )
+            )
         return value
 
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
-    
+
     def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
