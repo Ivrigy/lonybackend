559008dee97370fe9a0a4332ba4a6d36dc10b321
“Ivona” <ivona.rieger@gmail.com>
2025-06-02 17:12:04 +0200

    add text search

--- posts/views.py
+++ posts/views.py
@@ -9,16 +9,22 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("comment", distinct=True),
+    ).order_by("-created_at")
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
     ]
+    search_fields = [
+        "owner__username",
+        "title",
+    ]
+
     ordering_fields = [
-        'likes_count',
-        'comments_count',
-        'likes__created_at',
+        "likes_count",
+        "comments_count",
+        "likes__created_at",
     ]
 
     def perform_create(self, serializer):
@@ -29,6 +35,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("comment", distinct=True),
+    ).order_by("-created_at")
