414f042694a943f9e26800cd72934e31aebcd58d
“Ivona” <ivona.rieger@gmail.com>
2025-07-24 18:26:40 +0200

    fix image storage path

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    image = serializers.ReadOnlyField(source="image.url")
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
@@ -16,11 +17,9 @@ class PostSerializer(serializers.ModelSerializer):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError("Image size larger than 2MB!")
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                "Image height larger than 4096px!")
+            raise serializers.ValidationError("Image height larger than 4096px!")
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                "Image width larger than 4096px!")
+            raise serializers.ValidationError("Image width larger than 4096px!")
         return value
 
     def get_is_owner(self, obj):
@@ -28,11 +27,9 @@ class PostSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     def get_like_id(self, obj):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
+            like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
 
