f009497a30a08ccc5117ed1028ee74e93aa23821
“Ivona” <ivona.rieger@gmail.com>
2025-07-25 00:25:15 +0200

    fix strorage path

--- drf_api/wsgi.py
+++ drf_api/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+import django.core.files.storage
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api.settings')
+
+django.core.files.storage.default_storage = MediaCloudinaryStorage()
 
 application = get_wsgi_application()
