f02841d881b37656d5d59a0af6c3db2ef9fa29f3
“Ivona” <ivona.rieger@gmail.com>
2025-07-25 00:37:40 +0200

    try force Cloudinary storage in wsgi.py

--- drf_api/wsgi.py
+++ drf_api/wsgi.py
@@ -6,13 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+
 import os
-from django.core.wsgi import get_wsgi_application
-import django.core.files.storage
+
+from django.core.files.storage import default_storage
 from cloudinary_storage.storage import MediaCloudinaryStorage
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf_api.settings")
 
-django.core.files.storage.default_storage = MediaCloudinaryStorage()
+default_storage._wrapped = MediaCloudinaryStorage()
 
 application = get_wsgi_application()
